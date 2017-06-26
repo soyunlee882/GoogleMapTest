27a1f88060df1ea435e6f860da16ac63ad3ee7b8
soyunlee882 <soyunlee882@gmail.com>
2017-06-26 20:53:17 +0900

    #4OnMapReadyCallback and camera

--- app/src/main/java/syl/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -3,11 +3,36 @@ package syl.kr.hs.emirim.googlemaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.486544,127.005805),17));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+
+
     }
 }
